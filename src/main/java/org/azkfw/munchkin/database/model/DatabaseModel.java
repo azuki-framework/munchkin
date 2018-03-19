0a0eab7c1fea23af1cc10ea4a9aa158cf23a0243
kawakicchi <kawakicchi@gmail.com>
2018-03-19 17:26:52 +0900

    Add javadoc.

--- src/main/java/org/azkfw/munchkin/database/model/DatabaseModel.java
+++ src/main/java/org/azkfw/munchkin/database/model/DatabaseModel.java
@@ -45,13 +45,46 @@ public interface DatabaseModel {
 
 	List<Map<String, Object>> executeQuery(String sql, List<Object> params) throws SQLException;
 
+	/**
+	 * デフォルトスキーマ情報を取得する。
+	 *
+	 * @return スキーマ情報
+	 * @throws SQLException SQL操作に起因する問題が発生した場合
+	 */
 	SchemaEntity getDefaultSchema() throws SQLException;
 
+	/**
+	 * スキーマ情報一覧を取得する。
+	 *
+	 * @return スキーマ情報一覧
+	 * @throws SQLException SQL操作に起因する問題が発生した場合
+	 */
 	List<SchemaEntity> getSchemaList() throws SQLException;
 
+	/**
+	 * タイプ情報一覧を取得する。
+	 *
+	 * @return タイプ情報一覧
+	 * @throws SQLException SQL操作に起因する問題が発生した場合
+	 */
 	List<TypeEntity> getTypeList() throws SQLException;
 
+	/**
+	 * オブジェクト情報一覧を取得する。
+	 *
+	 * @param schema スキーマ情報
+	 * @param type タイプ情報
+	 * @return オブジェクト情報一覧
+	 * @throws SQLException SQL操作に起因する問題が発生した場合
+	 */
 	List<ObjectEntity> getObjectList(SchemaEntity schema, TypeEntity type) throws SQLException;
 
+	/**
+	 * オブジェクトの詳細情報を取得する。
+	 *
+	 * @param object オブジェクト情報
+	 * @return オブジェクト詳細情報
+	 * @throws SQLException SQL操作に起因する問題が発生した場合
+	 */
 	ObjectDetailEntity getObjectDetail(ObjectEntity object) throws SQLException;
 }
